8e15d04376fe7c409dbe6b2141fe70f838ad13d1
Vanya.Aziz@haw-hamburg.de <Relativ1relativ>
2023-11-10 16:31:34 +0100

    Finished reparameterization trick with implicit reparameterization for class MixtureSameFamilyMod.

--- dsacv02/gmm_reparameterization/normal_stable.py
+++ dsacv02/gmm_reparameterization/normal_stable.py
@@ -4,51 +4,70 @@ from torch.distributions.normal import Normal
 
 
 class NormalStable(Normal):
+    """
+    Add stable cdf for implicit reparametrization, and stable _log_cdf.
+    """
 
-    def _standardize(self, x):
-        return (x - self.loc) * self.scale.reciprocal()
-
+    # Override default
     def cdf(self, value):
         if self._validate_args:
             self._validate_sample(value)
-        return ndtr(self._standardize(value))
+        return ndtr(self._standardise(value))
+
+    # NOTE: This is not necessary for implicit reparam.
+    def _log_cdf(self, value):
+        if self._validate_args:
+            self._validate_sample(value)
+        return log_ndtr(self._standardise(value))
+
+    def _standardise(self, x):
+        return (x - self.loc) * self.scale.reciprocal()
 
 
-'''
-Based on implementations in Scipy, Tensorflow Probability and issues in  
-https://github.com/pytorch/pytorch/issues/52973#issuecomment-787587188
-'''
-
-# Lower boud values. Chosen by observing where support of ndtr appears to be zero. Then made more safe by expansion
-LOGNDTR_FLOAT64_LOWER = -20.
-LOGNDTR_FLOAT32_LOWER = -10.
-
-# Upper bound values chosen by which value sof 'x' log[cdf(x)]=0, after which the appr Log[1-cdf(-x)] is used
-LOGNDTR_FLOAT64_UPPER = 8.
-LOGNDTR_FLOAT32_UPPER = 5.
-
+#
+# Below are based on the investigation in https://github.com/pytorch/pytorch/issues/52973#issuecomment-787587188
+# and implementations in SciPy and Tensorflow Probability
+#
 
 def ndtr(value: torch.Tensor):
     """
-    - Gaussian CDF
-    - erfc(x) = 1 - erf(x)
-    - erf(x) = 2/(\sqrt{\pi}) \int_0^{x}{e^{-t^2}dt}
-    :param value: Value up to which the integral is calculated
+    Standard Gaussian cumulative distribution function.
+    Based on the SciPy implementation of ndtr
+    https://github.com/scipy/scipy/blob/master/scipy/special/cephes/ndtr.c#L201-L224
     """
     sqrt_half = torch.sqrt(torch.tensor(0.5, dtype=value.dtype))
     x = value * sqrt_half
     z = torch.abs(x)
     y = 0.5 * torch.erfc(z)
-    output = torch.where(z < sqrt_half, 0.5 + 0.5 * torch.erf(x), torch.where(x > 0, 1-y, y))
-
+    output = torch.where(z < sqrt_half,
+                         0.5 + 0.5 * torch.erf(x),
+                         torch.where(x > 0, 1 - y, y))
     return output
 
 
-def log_ndtr(value: torch.tensor):
+# log_ndtr uses different functions over the ranges
+# (-infty, lower](lower, upper](upper, infty)
+# Lower bound values were chosen by examining where the support of ndtr
+# appears to be zero, relative to scipy's (which is always 64bit). They were
+# then made more conservative just to be safe. (Conservative means use the
+# expansion more than we probably need to.)
+LOGNDTR_FLOAT64_LOWER = -20.
+LOGNDTR_FLOAT32_LOWER = -10.
+
+# Upper bound values were chosen by examining for which values of 'x'
+# Log[cdf(x)] is 0, after which point we need to use the approximation
+# Log[cdf(x)] = Log[1 - cdf(-x)] approx -cdf(-x). We chose a value slightly
+# conservative, meaning we use the approximation earlier than needed.
+LOGNDTR_FLOAT64_UPPER = 8.
+LOGNDTR_FLOAT32_UPPER = 5.
+
+
+def log_ndtr(value: torch.Tensor):
     """
-    - Standard Gaussian log-cumulative distribution function
-    - Based on TFP and SciPy implementations
-    :param value: Value up to which the integral is calculated
+    Standard Gaussian log-cumulative distribution function.
+    This is based on the TFP and SciPy implementations.
+    https://github.com/tensorflow/probability/blame/master/tensorflow_probability/python/internal/special_math.py#L156-L245
+    https://github.com/scipy/scipy/blob/master/scipy/special/cephes/ndtr.c#L316-L345
     """
     dtype = value.dtype
     if dtype == torch.float64:
@@ -56,29 +75,38 @@ def log_ndtr(value: torch.tensor):
     elif dtype == torch.float32:
         lower, upper = LOGNDTR_FLOAT32_LOWER, LOGNDTR_FLOAT32_UPPER
     else:
-        raise TypeError(f'dtype={dtype} is not supported')
+        raise TypeError(f'dtype={value.dtype} is not supported.')
 
-    # When x < lower, perform a fixed series asymptotic expansion,
-    conditonal_inner = torch.where(value >= lower, torch.log(ndtr(value)), log_ndtr_series(value))
-    output = torch.where(value > upper, -ndtr(-value), conditonal_inner)
+    # When x < lower, then we perform a fixed series expansion (asymptotic)
+    # = log(cdf(x)) = log(1 - cdf(-x)) = log(1 / 2 * erfc(-x / sqrt(2)))
+    # = log(-1 / sqrt(2 * pi) * exp(-x ** 2 / 2) / x * (1 + sum))
+    # When x >= lower and x <= upper, then we simply perform log(cdf(x))
+    # When x > upper, then we use the approximation log(cdf(x)) = log(1 - cdf(-x)) \approx -cdf(-x)
+    # The above approximation comes from Taylor expansion of log(1 - y) = -y - y^2/2 - y^3/3 - y^4/4 ...
+    # So for a small y the polynomial terms are even smaller and negligible.
+    # And we know that for x > upper, y = cdf(x) will be very small.
+    return torch.where(value > upper,
+                       -ndtr(-value),
+                       torch.where(value >= lower,
+                                   torch.log(ndtr(value)),
+                                   log_ndtr_series(value)))
 
-    return output
 
-
-def log_ndtr_series(value: torch.tensor, num_terms=3):
+def log_ndtr_series(value: torch.Tensor, num_terms=3):
     """
-    - Asymptotic series expansion of log of normal CDF at value
-    - Based on SciPy implementation
-    :param value: Integral up to value ofr CDF
-    :param num_terms: Number of expansions
+    Function to compute the asymptotic series expansion of the log of normal CDF
+    at value.
+    This is based on the SciPy implementation.
+    https://github.com/scipy/scipy/blob/master/scipy/special/cephes/ndtr.c#L316-L345
     """
-    value_sq = value**2
-    t1 = -0.5 * (np.log(2*np.pi) + value_sq) - torch.log(-value)
+    # sum = sum_{n=1}^{num_terms} (-1)^{n} (2n - 1)!! / x^{2n}))
+    value_sq = value ** 2
+    t1 = -0.5 * (np.log(2 * np.pi) + value_sq) - torch.log(-value)
     t2 = torch.zeros_like(value)
     value_even_power = value_sq.clone()
     double_fac = 1
     multiplier = -1
-    for n in range(1, num_terms+1):
+    for n in range(1, num_terms + 1):
         t2.add_(multiplier * double_fac / value_even_power)
         value_even_power.mul_(value_sq)
         double_fac *= (2 * n - 1)
@@ -89,9 +117,6 @@ def log_ndtr_series(value: torch.tensor, num_terms=3):
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import scipy.special as ss
-    import os
-
-    os.environ['KMP_DUPLICATE_LIB_OK'] = "TRUE"
 
     x = torch.linspace(-30, 10, 40000, dtype=torch.float32)
     out = log_ndtr(x)
@@ -118,5 +143,3 @@ if __name__ == "__main__":
     plt.title('Float64')
     plt.legend()
     plt.show()
-
-
--- dsacv02/gmm_reparameterization/test_implicit_repara.py
+++ dsacv02/gmm_reparameterization/test_implicit_repara.py
@@ -1,10 +1,44 @@
-from mixture_same_family import *
-from normal_stable import *
+import torch
+import torch.distributions as distr
+from mixture_same_family import ReparameterizedMixtureSameFamilyMod
+from normal_stable import NormalStable
+from math import prod
 
+n_kernels = 2
+mixture_probs = torch.ones(n_kernels) / n_kernels
+sample_shape = (5,)
 
-if __name__ == '__main__':
-    cat_distr = distr.Categorical(probs=torch.tensor([0.5, 0.5]))
-    component_distr = distr.Normal(loc=torch.tensor([-1.0, 1.0]), scale=torch.tensor([1.0, 1.0]))
-    mix_mod = MixtureSameFamilyMod(mixture_distribution=cat_distr, component_distribution=component_distr)
+locs = torch.tensor([-1., 1.])
+stds = torch.ones(n_kernels)
 
+mixture1 = distr.Categorical(probs=mixture_probs)
+components1 = NormalStable(loc=locs, scale=stds)
+gmm1 = ReparameterizedMixtureSameFamilyMod(mixture_distribution=mixture1,
+                                          component_distribution=components1)
 
+extra_ndims = 2
+components2 = NormalStable(loc=locs.reshape(locs.shape + (1,) * extra_ndims),
+                           scale=stds.reshape(stds.shape + (1,) * extra_ndims))
+components2 = distr.Independent(components2, extra_ndims)
+gmm2 = ReparameterizedMixtureSameFamilyMod(mixture_distribution=mixture1,
+                                           component_distribution=components2)
+torch.manual_seed(123456)
+X1 = gmm1.rsample(sample_shape=(5,))
+Z1 = gmm1._distributional_transform(X1)
+
+# torch.manual_seed(123456)
+# X2 = gmm2.rsample(sample_shape=(5,))
+# Z2 = gmm2._distributional_transform(X2)
+#
+# assert torch.allclose(X1, X2.squeeze())
+# assert torch.allclose(Z1, Z2.squeeze())
+
+# # Check if multivariate runs
+# mixture3 = distr.Categorical(probs=mixture_probs[0])
+# components3 = NormalStable(loc=locs.T, scale=stds.T)
+# components3 = distr.Independent(components3, 1)
+# mog3 = ReparameterizedMixtureSameFamilyMod(mixture_distribution=mixture3,
+#                                            component_distribution=components3)
+#
+# X3 = mog3.rsample(sample_shape=(5,))
+# # TODO: add test for multivariate
