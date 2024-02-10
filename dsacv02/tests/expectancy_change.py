Corrected actor loss calculation.

dsacv02/tests/expectancy_change.py
@@ -47,9 +47,9 @@ def cramer_py_test(pdf_target: torch.tensor, pdf_curr: torch.tensor, int_l, int_
     cramer = torch.trapz((dy_target_cdf - dy_curr_cdf)**2, dx=spacing)
 
     # return cramer**0.5
-    return torch.log(cramer)
+    # return torch.log(cramer)
     # return cramer
-
+    return cramer**2
 
 def generate_gmm(locs: torch.tensor, scales: torch.tensor, kweights: torch.tensor):
     # Test mysterious symmetry