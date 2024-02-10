Corrected actor loss calculation.

algo_concept_tests/cramer.py
@@ -33,7 +33,7 @@ def energy_d(supports, p_target, p_curr, s_size):
     diff_t_t = np.abs(t_samples - t_samples_prime)
     diff_c_c = np.abs(samples - samples_prime)
     diff_t_c = np.abs(t_samples - samples)
-    e_distance = 2 * diff_t_c.mean_target() - diff_t_t.mean_target() - diff_c_c.mean_target()
+    e_distance = 2 * diff_t_c.mean() - diff_t_t.mean() - diff_c_c.mean()
 
     return e_distance
 
@@ -126,7 +126,7 @@ if __name__ == '__main__':
     print(f"Energy Distance: {ed}")
 
     # m
-    S_SIZE = 1100
+    S_SIZE = 11000
     MIN_SUPPORT = -20
     MAX_SUPPORT = 20
     supports = np.arange(MIN_SUPPORT, MAX_SUPPORT, 0.02)
@@ -162,6 +162,7 @@ if __name__ == '__main__':
         print(f'Own implementation of energy distance: {l_i_own}, its square root: {l_i_own**0.5}')
         l_cramer_pdf, err = cramer_from_pdf(target_pdf_cont, pdfs_cont[idx], int_l=-50, int_u=50)
         print(f'Cramer distance from PDF: {l_cramer_pdf} with max. error: {err}')
+        print('----------------------------------------------------------')
 
     # Define a GMM in 1d with n Kernels in PyTorch
     means = torch.arange(-10.0, 11.0, 1.0)

dsacv02/agentv02.py
@@ -50,6 +50,7 @@ class Agent:
         :param memory_size: Max. replay buffer size
         :param device: Device on which networks are running
         """
+        self.n_kernels = n_kernels
         self.batch_size = batch_size
         self.t_max = t_max
         self.tau = tau
@@ -63,18 +64,18 @@ class Agent:
 
         self.q1: nn.Module = Critic(state_dim=obs_dim, action_dim=action_dim, value_min_std=value_min_std,
                                     value_max_std=value_max_std, hidden_layers=cr_hl, activ=cr_activ, device=device,
-                                    learnable_weights=learnable_kweights)
+                                    learnable_weights=learnable_kweights, n_kernels=self.n_kernels)
         self.q2: nn.Module = Critic(state_dim=obs_dim, action_dim=action_dim, value_min_std=value_min_std,
                                     value_max_std=value_max_std, hidden_layers=cr_hl, activ=cr_activ, device=device,
-                                    learnable_weights=learnable_kweights)
+                                    learnable_weights=learnable_kweights, n_kernels=self.n_kernels)
         self.q1_target: nn.Module = deepcopy(self.q1)
         self.q2_target: nn.Module = deepcopy(self.q2)
         self.policy: nn.Module = Actor(state_dim=obs_dim, action_dim=action_dim, hidden_layers=act_hl,
                                        activation=act_activ, action_min_std=act_min_std, action_max_std=act_max_std,
                                        action_low_lim=action_low, action_up_lim=action_up, device=device,
-                                       learnable_weights=learnable_kweights)
+                                       learnable_weights=learnable_kweights, n_kernels=self.n_kernels)
         self.policy_target = deepcopy(self.policy)
-        self.log_alpha = nn.Parameter(torch.tensor(log_alpha_ini, dtype=torch.float32, device=device))
+        self.log_alpha = nn.Parameter(torch.tensor(log_alpha_ini, dtype=torch.float64, device=device))
 
         self.dsac = RealDSAC(critic1=self.q1, critic2=self.q2, critic1_target=self.q1_target,
                              critic2_target=self.q2_target, cr_lr_ini=cr_lr_ini, cr_lr_fin=cr_lr_fin,
@@ -180,7 +181,8 @@ class Agent:
             complete_tar_file
         )
         # Save training and algorithm data
-        agent_meta_data = (self.batch_size, self.t_max, self.tau, self.static_alpha, self.reward_scale, self.gamma,
+        agent_meta_data = (self.batch_size, self.n_kernels, self.t_max, self.tau, self.static_alpha, self.reward_scale,
+                           self.gamma,
                            self.update_interval, self.auto_alpha)
         actor_meta_params = self.policy.get_class_info()
         critic_class_params = self.q1.get_class_info()

dsacv02/algov02.py
@@ -5,7 +5,7 @@ import time
 from dsacv02.gmm_reparameterization.mixture_same_family import ReparameterizedMixtureSameFamilyMod as RMM
 from torch.optim import Adam, lr_scheduler
 from dsac_old_versions.dsac_implementation.tensorboard_tools import tb_tags
-from dsacv02.tools import cramer_from_pdf, cramer_torch, approx_integral_bounds, get_double_q_selections, \
+from dsacv02.tools import cramer_torch, approx_integral_bounds, get_double_q_selections, \
      get_partial_double_q_selections
 
 
@@ -295,7 +295,7 @@ class RealDSAC:
                 self.evaluate_z(obs=states_next, actions=actions_bounded_next, znet=self.q2_target,
                                 exp=exp, sample=False, reparameterize=True)
 
-            means_min, means_next_min, stds_selected_min, stds_next_selected_min, kweights_selected_min, \
+            means_min, means_next_min, stds_selected_min, stds_next_selected_min, kweights, \
                 kweights_next_selected_min = get_double_q_selections(means1=means1, means2=means2,
                                                                      means1_next=means1_next,
                                                                      means2_next=means2_next, stds1=stds1, stds2=stds2,
@@ -305,7 +305,7 @@ class RealDSAC:
                                                                      kweights2_next=kweights2_next)
 
             # Calculate current distribution
-            zcal = self.generate_gmm_distr(means_min, stds_selected_min, kweights_selected_min)
+            zcal = self.generate_gmm_distr(means_min, stds_selected_min, kweights)
             z = zcal.rsample(sample_shape=batch.shape[0])
 
             # Calculate target distribution
@@ -325,7 +325,7 @@ class RealDSAC:
         else:
             # Calculate current and target distributions, NOTE: Evaluation for \mathcal{Z}(|,s',a') already done
             # before If-statement
-            z, zcal, means, stds, kernel_weights = self.evaluate_z(obs=states, actions=old_actions, znet=self.q1,
+            z, zcal, means, stds, kweights = self.evaluate_z(obs=states, actions=old_actions, znet=self.q1,
                                                                    exp=exp, sample=True, reparameterize=True)
             zcal_next, z_next = self.compute_target_distribution(rewards=rewards, dones=dones, q_means_next=means1_next,
                                                                  stds_next=stds1_next, kernel_weights=kweights1_next,
@@ -341,9 +341,9 @@ class RealDSAC:
 
         # Calculate loss with batch-sensitive Cràmer distance on PDFs
         q_loss = cramer_torch(pdf_target=zcal_next, pdf_curr=zcal, int_l=int_bound_low, int_u=int_bound_up,
-                              spacing=1e-3, dev=self.device)
+                              spacing=1e-2, dev=self.device)
 
-        return q_loss.mean(), means.mean(), stds.mean(), kernel_weights[:, 0].mean(), kernel_weights[:, 1].mean()
+        return q_loss.mean(), means.mean(), stds.mean(), kweights
 
     def compute_policy_loss(self, states, actions_curr_pol, log_ps_curr_pol, double_q=False, exp=False):
         """
@@ -383,8 +383,9 @@ class RealDSAC:
             stds_selected_min = stds_selected_min.detach()
             kweights_selected_min = kweights_selected_min.detach()
 
+        gmm_mean = (means_min * kweights_selected_min).sum(dim=1)
+
         # Not calculated according to Z! If Z, reparameterization is needed
-        gmm_mean = means_min.mean(dim=1)
         gmm_mean.unsqueeze_(dim=1)
 
         policy_loss = (self.get_alpha(requires_grad=False) * log_ps_curr_pol - gmm_mean).mean()
@@ -425,8 +426,8 @@ class RealDSAC:
         self.q2_optimizer.zero_grad()
 
         # Compute Z-Loss, NOTE: Check exponentiation
-        loss_q, mean_q, std_mean, k1_mean, k2_mean = self.compute_z_loss(batch=batch, double_q=False,
-                                                                         integral_bound_factor=5, exp=False)
+        loss_q, mean_q, std_mean, kweights_cr = self.compute_z_loss(batch=batch, double_q=False,
+                                                                    integral_bound_factor=4, exp=False)
         loss_q.backward()
 
         loss_policy, entropy = None, None
@@ -451,18 +452,18 @@ class RealDSAC:
         tb_info = {
             "DSAC2_Vals/gmm_critic_avg_value iter": mean_q.detach().item(),
             "DSAC2_CrDistr/gmm_critic_avg_std iter": std_mean.detach().item(),
-            "DSAC2_CrDistr/gmm_critic_avg_k1_weight iter": k1_mean.detach().item(),
-            "DSAC2_CrDistr/gmm_critic_avg_k2_weight iter": k2_mean.detach().item(),
             "DSAC2_Vals/gmm_actor_avg_action iter": policy_mean,
             "DSAC2_ActDistr/gmm_actor_avg_std iter": policy_std,
-            "DSAC2_ActDistr/gmm_actor_avg_k1_weight iter": kweights_act[:, 0].mean().detach().item(),
-            "DSAC2_ActDistr/gmm_actor_avg_k2_weight iter": kweights_act[:, 1].mean().detach().item(),
             "DSAC2_ActDistr/entropy-RL iter": entropy.detach().item(),
             "DSAC2_Alpha/alpha-RL iter": self.get_alpha(requires_grad=False),
             tb_tags["loss_actor"]: loss_policy.detach().item(),
             tb_tags["loss_critic"]: loss_q.detach().item(),
             tb_tags["alg_time"]: (time.time() - start_time) * 1000,
         }
+
+        for i in range(self.n_kernels):
+            tb_info[f"DSAC2_ActDistr/gmm_actor_avg_k{i+1}_weight iter"] = kweights_act[:, i].mean().detach().item()
+            tb_info[f"DSAC2_CrDistr/gmm_critic_avg_k{i+1}_weight iter"] = kweights_cr[:, i].mean().detach().item()
 
         return tb_info
 

dsacv02/tests/cramer_large_distance.py
@@ -138,14 +138,12 @@ if __name__ == '__main__':
     kweights_fix.to(device)
 
     # Train parameters
-    mb_size = 10
-    epochs_ref = 1
-    epochs_tar = 5
-    epochs_low_e = 1
-    epochs_high_e = 1
+    mb_size = 64
+    epochs_ref = 2
+    epochs_tar = 6
 
     # Initialize input
-    n_datapoints = 10000
+    n_datapoints = 64000
     n_mb = int(n_datapoints / mb_size)
     input_total = torch.randn(size=(n_datapoints, 1)).to(device)
     batches = input_total.view(n_mb, mb_size, 1)
@@ -157,7 +155,8 @@ if __name__ == '__main__':
     distr_ref = generate_gmm(locs=mean_ref, scales=std_ref, kweights=kweight_ref)
 
     # Target distribution
-    mean_tar = (torch.ones(size=(mb_size, 1)) * torch.tensor([5.0, 30.0], dtype=torch.float64)).to(device)
+    mean_tar = (torch.ones(size=(mb_size, 1)) * torch.tensor([5.0, 6.0], dtype=torch.float64)).to(device)
+    # mean_tar = (torch.ones(size=(mb_size, 1)) * torch.tensor([5.0, 50.0], dtype=torch.float64)).to(device)
     std_tar = (torch.ones(size=(mb_size, 1)) * torch.tensor([1.0, 1.0], dtype=torch.float64)).to(device)
     kweight_tar = (torch.ones(size=(mb_size, 1)) * torch.tensor([0.5, 0.5], dtype=torch.float64)).to(device)
     distr_tar = generate_gmm(locs=mean_tar, scales=std_tar, kweights=kweight_tar)
@@ -210,7 +209,7 @@ if __name__ == '__main__':
     '''
     print('\n --------------------------- \n')
     gmm_tar = deepcopy(gmm_ref)
-    optimizer_tar = optim.Adam(gmm_tar.parameters(), lr=0.001)
+    optimizer_tar = optim.Adam(gmm_tar.parameters(), lr=0.01)
 
     for epoch in range(epochs_tar):
         for batch in batches:
@@ -230,7 +229,7 @@ if __name__ == '__main__':
             l_tar.detach_().item()
             u_tar.detach_().item()
             cramer_py_loss_tar_i = cramer_distance(pdf_target=distr_tar, pdf_curr=pred_gmm_tar_i, int_l=l_tar,
-                                                   int_u=u_tar, spacing=0.01, dev=device)
+                                                   int_u=u_tar, spacing=0.001, dev=device)
 
             # Loss in SGD for cramer_py_loss_i
             optimizer_tar.zero_grad()

dsacv02/tests/expectancy_change.py
@@ -46,7 +46,9 @@ def cramer_py_test(pdf_target: torch.tensor, pdf_curr: torch.tensor, int_l, int_
 
     cramer = torch.trapz((dy_target_cdf - dy_curr_cdf)**2, dx=spacing)
 
-    return cramer**0.5
+    # return cramer**0.5
+    return torch.log(cramer)
+    # return cramer
 
 
 def generate_gmm(locs: torch.tensor, scales: torch.tensor, kweights: torch.tensor):
@@ -75,15 +77,15 @@ if __name__ == '__main__':
     distr_ref = generate_gmm(locs=mean_ref, scales=std_ref, kweights=kweight_ref)
 
     # High-entropy Distribution
-    mean_high_e = torch.tensor([2.0, 8.0], dtype=torch.float64).to(device)
-    std_high_e = torch.tensor([5.0, 5.0], dtype=torch.float64).to(device)
-    kweight_high_e = torch.tensor([0.2, 0.8], dtype=torch.float64).to(device)
+    mean_high_e = torch.tensor([2.0, 15.0], dtype=torch.float64).to(device)
+    std_high_e = torch.tensor([3.0, 3.0], dtype=torch.float64).to(device)
+    kweight_high_e = torch.tensor([0.5, 0.5], dtype=torch.float64).to(device)
     distr_high_e = generate_gmm(locs=mean_high_e, scales=std_high_e, kweights=kweight_high_e)
 
     # Low-entropy Distribution
-    mean_low_e = torch.tensor([2.0, 8.0], dtype=torch.float64).to(device)
+    mean_low_e = torch.tensor([2.0, 15.0], dtype=torch.float64).to(device)
     std_low_e = torch.tensor([1.0, 1.0], dtype=torch.float64).to(device)
-    kweight_low_e = torch.tensor([0.2, 0.8], dtype=torch.float64).to(device)
+    kweight_low_e = torch.tensor([0.5, 0.5], dtype=torch.float64).to(device)
     distr_low_e = generate_gmm(locs=mean_low_e, scales=std_low_e, kweights=kweight_low_e)
 
     # # Calculate Distances
@@ -99,12 +101,18 @@ if __name__ == '__main__':
     kweights_fix = torch.ones(n_kernels, dtype=torch.float64) / n_kernels
     kweights_fix.to(device)
 
+    # Train parameters
+    learning_rates = 0.001
+    epochs = 4
+    epochs_low_e = 1
+    epochs_high_e = 1
+
     # Initialize network and optimizer
     if learnable_weights:
         gmm_approx = MLPGMMWeighted(arch=arch, activ=activ, n_kernels=n_kernels, device=device, multivar=multivar)
     else:
         gmm_approx = MLPGMM(arch=arch, activ=activ, n_kernels=n_kernels, device=device, multivar=multivar)
-    optimizer_ref = optim.Adam(gmm_approx.parameters(), lr=0.001)
+    optimizer_ref = optim.Adam(gmm_approx.parameters(), lr=learning_rates)
 
     # Initialize input
     n_datapoints = 6000
@@ -112,11 +120,6 @@ if __name__ == '__main__':
     n_mb = int(n_datapoints / mb_size)
     input_total = torch.randn(size=(n_datapoints, 1)).to(device)
     batches = input_total.view(n_mb, mb_size, 1)
-
-    # Train parameters
-    epochs = 4
-    epochs_low_e = 1
-    epochs_high_e = 1
 
     # Fit gmm_approx to output normal distributio
     for epoch in range(epochs):
@@ -166,7 +169,7 @@ if __name__ == '__main__':
     Measure change for low entropy target distribution
     #
     '''
-    optimizer_low = optim.Adam(gmm_approx_low_e.parameters(), lr=0.001)
+    optimizer_low = optim.Adam(gmm_approx_low_e.parameters(), lr=learning_rates)
     means_history_low_e_target = torch.tensor([], dtype=torch.float64, device=device)
     stds_history_low_e_target = torch.tensor([], dtype=torch.float64, device=device)
     dc_history_low_e_target = torch.tensor([], dtype=torch.float64, device=device)
@@ -213,7 +216,7 @@ if __name__ == '__main__':
     '''
     Measure change for high entropy target distribution
     '''
-    optimizer_high = optim.Adam(gmm_approx_high_e.parameters(), lr=0.001)
+    optimizer_high = optim.Adam(gmm_approx_high_e.parameters(), lr=learning_rates)
     means_history_high_e_target = torch.tensor([], dtype=torch.float64, device=device)
     stds_history_high_e_target = torch.tensor([], dtype=torch.float64, device=device)
     dc_history_high_e_target = torch.tensor([], dtype=torch.float64, device=device)

dsacv02/tests/expectancy_change_mlt.py
@@ -5,9 +5,9 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 curr_path = os.getcwd()
 main_dir = 'Learnable_Weights'
-dir_name = 'test'
-# loading_path = curr_path + '/' + main_dir + '/' + dir_name
-loading_path = curr_path + '/' + dir_name
+dir_name = 'r18_mod3_G'
+loading_path = curr_path + '/' + main_dir + '/' + dir_name
+# loading_path = curr_path + '/' + dir_name
 
 means_history_low = torch.load(loading_path + '/' + 'means_history_low').cpu().detach().numpy()
 stds_history_low = torch.load(loading_path + '/' + 'stds_history_low').cpu().detach().numpy()