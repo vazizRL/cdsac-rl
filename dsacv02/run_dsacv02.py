Completed LinEnv live overestimation tests. Begin fixing overestimation analysis tools with correcte formula.

dsacv02/run_dsacv02.py
@@ -9,9 +9,11 @@ from tools import smoothing
 
 ''' Environment constants '''
 # gym_env = 'Pendulum-v1'
-gym_env = 'CartPole-v1'
+gym_env = 'InvertedPendulum-v4'
+# gym_env = 'CartPole-v1'
 # gym_env = 'Hopper-v4'
 DEVICE = 'cuda:0'
+DISCRETE = False
 
 ''' Agent constants '''
 # Action Space for InvertedPendulum-v4
@@ -34,8 +36,8 @@ ACT_HL = (256, 256)
 CR_ACTIV = ('relu', 'relu')     # ('gelu', 'gelu')
 ACT_ACTIV = ('relu', 'relu')    # ('gelu', 'gelu')
 # Action boundaries
-ACTION_LOW = -1.0
-ACTION_HIGH = 1.0
+ACTION_LOW = -3.0
+ACTION_HIGH = 3.0
 # RL parameters
 DOUBLE_Q = True
 BATCH_SIZE = 256
@@ -43,7 +45,7 @@ T_MAX = 5000                     # Old 20000
 TAU = 0.015
 STATIC_ALPHA = 0.3              # Old 0.2
 REWARD_SCALE = 2.0              # Old 0.2
-GAMMA = 0.95                    # Old 0.99
+GAMMA = 0.98                    # Old 0.99
 UPDATE_INTERVAL = 1
 AUTO_ALPHA = True
 ALPHA_INI = 1                  # Old -1
@@ -119,7 +121,10 @@ if __name__ == '__main__':
         interval_reward = 0
         while not done:
             action, prob_action = agent.choose_action(observation)
-            action = 0 if action <= 0 else 1
+            if DISCRETE:
+                action = 0 if action <= 0 else 1
+            else:
+                action = action.squeeze(axis=1)
             observation_, reward, done, info, _ = env.step(action)
             observation_ = observation_.reshape((1, OBSERVATION_DIM))
             # observation_ = np.expand_dims(observation_, axis=0)

dsacv02/run_lin_analysis.py
@@ -13,14 +13,15 @@ from copy import deepcopy
 
 ''' Environment constants '''
 DEVICE = 'cuda:0'
-N_CELLS = 10
+N_CELLS = 50        # 10
 CELL_LIST = torch.tensor([[i] for i in range(N_CELLS)], device=DEVICE).unsqueeze(dim=2)
 ACTION_LEFT = torch.tensor([-1.0], device=DEVICE).unsqueeze(dim=1)
 ACTION_RIGHT = torch.tensor([1.0], device=DEVICE).unsqueeze(dim=1)
 RIGHT_GETS_REWARD = True
-STOCHASTICITY_TERMINAL = 0.1
+STOCHASTICITY_TERMINAL = 0.0
+CONTINUOUS = True
+
 ''' Agent constants '''
-# Action Space for InvertedPendulum-v4
 ACTION_DIM = 1
 OBSERVATION_DIM = 1
 N_KERNELS_ACT = 1
@@ -59,7 +60,7 @@ N_POL_UPDATE_INTERVAL = 1
 Training Parameters
 '''
 n_tot_steps = 1
-MAX_TOTAL_ITER = 12040               # 150000
+MAX_TOTAL_ITER = 32040               # 12040
 MAX_EPISODE_ITER = 500
 LOG_Y_DIFF_INTERVAL = 4000
 
@@ -217,8 +218,13 @@ if __name__ == '__main__':
         # Choose actions with DSAC and SAC
         action_dsac, _ = agent_dsac.choose_action(observation_dsac)
         action_sac = agent_sac.choose_action(observation_sac)
-        action_dsac = -1 if action_dsac <= 0 else 1
-        action_sac = -1 if action_sac <= 0 else 1
+
+        if CONTINUOUS:
+            action_dsac = action_dsac.item()
+            action_sac = action_sac.item()
+        else:
+            action_dsac = -1 if action_dsac <= 0 else 1
+            action_sac = -1 if action_sac <= 0 else 1
 
         # Get experience with DSAC and SAC and reshape
         observation_dsac_next, reward_dsac, done_dsac, info_dsac, _ = env_dsac.step(action_dsac)