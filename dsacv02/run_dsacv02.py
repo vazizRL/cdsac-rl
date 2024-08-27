38e0ecffb2d462b3725fb770cd49a80e6c884539
Vanya.Aziz@haw-hamburg.de <Relativ1relativ>
2024-08-27 16:54:14 +0200

    Reset CrMax to 1e3 in run_dsacv02.py and run_dsacv02_collab.py.

--- dsacv02/run_dsacv02.py
+++ dsacv02/run_dsacv02.py
@@ -29,7 +29,7 @@ CR_LR_INI, ACT_LR_INI, ALPHA_LR_INI = 3e-4, 3e-4, 3e-4
 CR_LR_FIN, ACT_LR_FIN, ALPHA_LR_FIN = 3e-4, 3e-4, 3e-4      # 6e-4, 6e-4, 6e-4
 # Standard deviations
 EXPONENTIATE = False
-CR_MIN_STD, CR_MAX_STD = 0.01, 25.0           # 0.01, 100.0
+CR_MIN_STD, CR_MAX_STD = 0.01, 1000.0           # 0.01, 100.0
 ACT_MIN_STD, ACT_MAX_STD = 1e-6, 1.0
 # Hidden Layers
 CR_HL = (256, 256)
