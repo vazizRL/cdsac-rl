7fac16c692312c4d1312eb663dcf051a831bb556
zardasht <vanya.aziz@gmx.de>
2025-03-26 19:04:26 +0100

    Fixed tools_ad.py

--- dsacv02/tools_ad.py
+++ dsacv02/tools_ad.py
@@ -32,7 +32,7 @@ def save_tensorboard_graphs(logdir, output_dir, n_kernels_act=1, n_kernels_cr=1)
                    'Loss/Actor loss-RL iter', 'Loss/Critic loss-RL iter', 'Rewards/Reward_Eval',
                    'Rewards/Reward_Training', 'DSAC2_Grads/actor_grad', 'Time/Algorithm time [ms]-RL iter'
                    ]
-    file_save_names = ['Actor_Loss.png', *kernel_names_act_output, 'Actor_Std.png', 'Alpha_Val.png',
+    file_save_names = ['Actor_Entropy.png', *kernel_names_act_output, 'Actor_Std.png', 'Alpha_Val.png',
                        *kernel_names_cr_output, 'Critic_Std.png', 'Actor_Val.png', 'Critic_Val.png', 'Actor_Loss.png',
                        'Critic_Loss.png', 'Reward_Eval.png', 'Reward_Training', 'Actor_Grad_Mag', 'Time_per_Iter.png']
 
@@ -71,7 +71,7 @@ def save_tensorboard_graphs(logdir, output_dir, n_kernels_act=1, n_kernels_cr=1)
 if __name__ == '__main__':
     N_KERNELS_ACT = 1
     N_KERNELS_CR = 1
-    curr_dir = r"C:\Users\vanya\OneDrive\Desktop\PhD_RL\RL_Framework\dsacv02\tests\DSAC_Runs_Optim_II\pol_imp_scale_std"
+    curr_dir = r"/home/zardasht/PycharmProjects/RL/dsacv02/tests/DSAC_Runs_Optim_II/auto_alpha_ini-2.9"
     output_dir = curr_dir + r"/graphs"
     plt.rcParams['figure.figsize'] = (30, 12)           # Old: (20, 8)
     # Example usage:
