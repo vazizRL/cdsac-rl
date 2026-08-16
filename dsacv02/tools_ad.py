d7a210fdd4ca2a3093fe86e9728d230b6d825ada
Vanya.Aziz@haw-hamburg.de <Relativ1relativ>
2026-08-16 19:14:38 +0200

    Adopted changed from cramer_single_adam_mod in tools_ad.py for fixing tb concatenation.

--- dsacv02/tools_ad.py
+++ dsacv02/tools_ad.py
@@ -272,12 +272,11 @@ def compute_avg_rewards(logdir, output_dir):
     print('Finished')
 
 
-def concatenate_tb_logs(ev_path, offset=0):
-    writer = SummaryWriter(ev_path, flush_secs=20)
-    ev_accs_path = find_event_files(ev_path, pattern_prefix="events.out.tfevents")
+def concatenate_tb_logs(ev_accs_path, output_dir, offset=0):
+    writer = SummaryWriter(output_dir)
     for ev_acc_path_i in ev_accs_path:
         ev_acc_i = event_accumulator.EventAccumulator(ev_acc_path_i,
-                                                     # size_guidance={'scalars': 0},
+                                                     size_guidance={'scalars': 0},
                                                      compression_bps=None)
         ev_acc_i.Reload()
         for tag in ev_acc_i.Tags().get("scalars", []):
@@ -310,5 +309,6 @@ if __name__ == '__main__':
     #                std_thld=std_threshold_cons, alpha=alpha)
     # compute_avg_rewards(logdir=log_path, output_dir=output_path_lib_avg)
 
-    concatenate_tb_logs(ev_path='/home/zardasht/PycharmProjects/RL/dsacv02/event_1786454529.165978/',
-                        offset=0)
+    # tb_events_path = [r"C:\Users\vanya\OneDrive\Desktop\PhD_RL\RL_Framework\dsacv02\event_1783802260.177389",
+    #              r"C:\Users\vanya\OneDrive\Desktop\PhD_RL\RL_Framework\dsacv02\event_1783802387.601975"]
+    # concatenate_tb_logs(tb_events_path, r"C:\Users\vanya\OneDrive\Desktop\PhD_RL\RL_Framework\dsacv02", offset=0)
